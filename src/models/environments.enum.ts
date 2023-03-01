b3b85e7bb1e0c0e520c3c701ffd02774e8427c11
MCantu-PieFi <104653422+MCantu-PieFi@users.noreply.github.com>
2023-03-01 13:04:34 -0700

    update

--- src/models/environments.enum.ts
+++ src/models/environments.enum.ts
@@ -2,7 +2,7 @@ enum PieFiConnectEnvironment {
   local='http://localhost:3001/',
   dev='https://connect-dev-api.upside.coop/',
   staging='https://connect-staging-api.upside.coop/',
-  prod='https://connect-prod-api.upside.coop/'
+  prod='https://connect-api.upside.coop/'
 }
 
 export default PieFiConnectEnvironment;
